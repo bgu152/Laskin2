5a0a12c03b97c8230d5efcb07e3b778b8ecbe63c
bgu152 <erikrammschmidt@gmail.com>
2021-11-08 11:09:33 +0200

    second commit

--- App.js
+++ App.js
@@ -1,21 +1,81 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import useState from 'react';
+import { StyleSheet, Text, View, Button, Alert, TextInput} from 'react-native';
 
 export default function App() {
+  const [resultText, setResultText] = React.useState('Result: ');
+  const [number1, setNumber1 ]=React.useState(null);
+  const [number2, setNumber2 ]=React.useState(null);
+  const [result, setResult] = React.useState(null);
+  const [history, setHistory] = React.useState('History\n');
+  const addition = ()=>{
+    const num=parseInt(number1)+parseInt(number2);
+    setResult(num);
+    setResultText('Result: ' + num);
+    setHistory(history + number1 + ' + ' + number2 +  ' = ' + num + '\n');
+  }
+  const subtraction = () => {
+    const num=parseInt(number1)-parseInt(number2);
+    setResult(num);
+    setResultText('Result: ' + num);
+    setHistory(history + number1 + ' - ' + number2 + ' = ' + num + '\n');
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.outerContainer}>
+    <View style={styles.inputContainer}>
+      <Text>{resultText}</Text>
+      <TextInput 
+      style ={styles.input}
+      onChangeText ={setNumber1}
+      placeholder ="number 1"
+      keyboardType="numeric"/>
       <StatusBar style="auto" />
+      <TextInput 
+      style ={styles.input}
+      onChangeText ={setNumber2}
+      placeholder ="number 2"
+      keyboardType="numeric"/>
+      <StatusBar style="auto" />
+    </View>
+    <View style={styles.buttonContainer}>
+    <Button  title = " + "   onPress = {addition}  />
+    <Button title = " - "  onPress = {subtraction} />   
+    </View>
+    <Text>{history}</Text>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  
+  inputContainer: {
+    flexDirection:'column',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  outerContainer: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 40,
+    width: 200,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  buttonContainer: {
+    
+    flexDirection:'row',
+    justifyContent: 'space-between',
+     width: 80, 
+    height: 40,
+  },
+    
 });
